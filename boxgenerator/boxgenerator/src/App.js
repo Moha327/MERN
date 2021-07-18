613fc76ef4d7265038a638676d5da7c0703e9a13
Moha327 <81511588+Moha327@users.noreply.github.com>
2021-07-18 15:59:58 +0300

    hello

---
+++ boxgenerator/boxgenerator/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Form from './components/Form';
+import Box from './components/Box';
+
+function App() {
+  const [boxes, setState] = useState([]);
+  return (
+    <div className="App">
+      <Form inputs={boxes} setInputs={setState}/>
+      {boxes.map((box, i) =>{
+        return(
+        <div key={i}>
+        <Box inputs={box}/>
+        </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
