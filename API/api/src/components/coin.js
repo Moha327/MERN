1fa7e7f10bff876c080eabce5c2472eab97a60af
Moha327 <81511588+Moha327@users.noreply.github.com>
2021-07-25 12:20:22 +0300

    hello

---
+++ API/api/src/components/coin.js
@@ -0,0 +1,28 @@
+import React ,{useState,useEffect} from "react";
+const Example = (props) => {
+    const [pokemon, setPeople] = useState([]);
+ const rem = (e) => {   
+   
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=807')
+            .then(response => response.json())
+            .then((x) => setPeople(x.results))
+           
+ };
+ 
+    return (
+        
+        <div>
+           
+                <button onClick={ rem}> get pokemons</button>
+              
+                {
+                pokemon.map((pokemon, index) => (
+                    <div key={ index }>
+                        <p>{ pokemon.name }</p>
+                    </div>
+                ) )
+            }
+        </div>
+    );
+};
+export default Example;
