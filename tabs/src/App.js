341247e54e834cf51a1121c5a04d11ff15ec80ef
Moha327 <81511588+Moha327@users.noreply.github.com>
2021-08-06 16:41:10 +0300

    hello

---
+++ tabs/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Tabss from "./components/tabss"
+function App() {
+  return (
+    
+    <div className="App">
+    <Tabss/>
+     </div>
+  );
+}
+
+export default App;
