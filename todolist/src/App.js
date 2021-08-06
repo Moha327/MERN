4bedd3c13f14df38697eea8b6da5d320505e10bc
Moha327 <81511588+Moha327@users.noreply.github.com>
2021-08-06 17:03:31 +0300

    hello

---
+++ todolist/src/App.js
@@ -0,0 +1,16 @@
+import React,{useState} from 'react';
+import Todo from './components/Todo';
+import './App.css';
+
+function App() {
+
+
+  return (
+  <div className="App">
+ <Todo/>
+
+  </div>
+  );
+}
+
+export default App;
